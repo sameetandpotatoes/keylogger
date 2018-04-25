da4e513e85ccf7a2a1c286b36875b5cd385aad95
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-24 20:24:58 -0500

    Set up db for heroku

--- src/server/database.py
+++ src/server/database.py
@@ -1,13 +1,20 @@
+import os
 from pymongo import MongoClient
 
 
 def setup_database():
-    global users, phrases, db, client
-    client = MongoClient()
-    reset_database()
+    global db, client
+
+    MONGO_URL = os.environ.get('MONGOHQ_URL')
+    if MONGO_URL:
+        # Heroku
+        client = MongoClient(MONGO_URL)
+    else:
+        # Local (Development)
+        client = MongoClient()
+        reset_database()
+
     db = client['keylogger']
-    users = db['users']
-    phrases = db['phrases']
 
 
 def reset_database():
@@ -15,23 +22,22 @@ def reset_database():
 
 
 def get_user_id(user):
-    # TODO Andrew: should we not store the image, and just store tags? and query by mac address (that's there now)
-    return users.find_one(user)
+    # TODO Andrew: should we not store the image, and just store tags? and query by mac address
+    return db.users.find_one(user)
 
 
 def get_users_by_os(os):
-    return users.find({"os":os})
+    return db.users.find({"os":os})
 
 
 def get_copied_phrases(n=100):
     # TODO Andrew: do group by phrase, and aggregate count, and limit to first n
-    return phrases.find({"copy_pastaed": True})
+    return db.phrases.find({"copy_pastaed": True})
 
 
 def insert_user(user):
-    global users
     user_dict = user.__dict__
-    return users.insert_one(user_dict)
+    return db.users.insert_one(user_dict)
 
 
 def insert_phrases(user, phrases_list):
@@ -41,7 +47,7 @@ def insert_phrases(user, phrases_list):
             user_id = insert_user(user)
         phrase = p.__dict__
         phrase['user_id'] = str(user_id)
-        phrases.insert_one(phrase)
+        db.phrases.insert_one(phrase)
 
 
-users, phrases, db, client = None, None, None, None
+db, client = None, None
